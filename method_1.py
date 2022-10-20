5cc300245fe9b23eee664b9de4e17140f15a5f65
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2022-10-20 13:06:05 +0100

    Update method_1.py

--- method_1.py
+++ method_1.py
@@ -1,11 +1,13 @@
 import csv
 
+# Open file adn read using csv library
 with open ('panda_data/survey_results_public.csv') as f:
     csv_reader = csv.DictReader(f)
 
     yes_count = 0
     no_count = 0
 
+# Iterate over lines in csv to categorise  
     for line in csv_reader:
         if line['Hobbyist'] == 'Yes':
             yes_count += 1
@@ -14,12 +16,13 @@ with open ('panda_data/survey_results_public.csv') as f:
 
 total = yes_count + no_count
 
+# Output percenbtages of yes and no
 yes_pct = (yes_count / total) * 100
 yes_pct = round(yes_pct, 2)
 
 no_pct = (no_count / total) * 100
 no_pct = round(no_pct, 2)
-
+# Print results
 print(f'Yes: {yes_pct}%')
 print(f'No: {no_pct}%')
 
